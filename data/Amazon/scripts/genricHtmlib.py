04bd5e353124284e47700c7f2909b93dbf57cafc
Paritosh Tripathi <tripathiparitosh935@gmail.com>
2023-04-01 19:43:25 +0530

    Data Scraping from Amazon

--- data/Amazon/scripts/genricHtmlib.py
+++ data/Amazon/scripts/genricHtmlib.py
@@ -119,7 +119,7 @@ class SeleniumScraper:
         
     def get_selenium_driver(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
